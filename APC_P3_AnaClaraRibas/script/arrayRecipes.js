6ad139fac3f6648c7832a42ee6e645e7f3d4934c
rfgomes <1151888@isep.ipp.pt>
2021-01-28 21:55:30 +0000

    init repository

---
+++ APC_P3_AnaClaraRibas/script/arrayRecipes.js
@@ -0,0 +1,94 @@
+﻿function listRecipes()
+{
+    // Initialize Array list
+    var list = [];
+
+    // Object Structure
+
+    // ingredients
+    var oliveOil = {
+        name: "Olive Oil",
+        quantity: "2 tbsp"
+    };
+
+    var beefMince = {
+        name: "Lean beef mince",
+        quantity: "750 g"
+    }
+
+    var prosciutto = {
+        name: "Pack prosciutto",
+        quantity: "90 g"
+    }
+
+    var tomatoSauce = {
+        name: "Tomato sauce",
+        quantity: "200 g"
+    }
+
+    var beefStock = {
+        name: "Hot beef stock",
+        quantity: "200 ml"
+    }
+
+    var nutmeg = {
+        name: "Nutmeg",
+        quantity: "2 g"
+    }
+
+    var freshLasagna = {
+        name: "Pack of fresh lasagne sheets",
+        quantity: "300 g"
+    }
+
+    var whiteSauce = {
+        name: "White sauce",
+        quantity: "300 g"
+    }
+
+    var mozzarella = {
+        name: "Ball mozzarella, torn into thin strips",
+        quantity: "150 g"
+    }
+
+    // Recipe
+    var lasagna = {
+        category: "Italian Cousine",
+        name: "Classic Lasagna",
+        ingredients: [oliveOil, beefMince, prosciutto, tomatoSauce, beefStock, nutmeg, freshLasagna, whiteSauce, mozzarella]
+        difficulty: "hard",
+        time: "70 minutes"
+    };
+
+    //add recipes
+    list.push(lasagna);
+
+    var output = "";
+
+    // create output
+    for (var i = 0; i < list.length; i++) {
+        var element = list[i];
+        output += "Recipe: " + element.name + "."
+        output += "Cousine: " + element.category + "."
+        output += "Dificulty: " + element.difficulty + "."
+
+        var ingredients = "ingredients { ";
+
+        for (var j = 0; j < element.ingredients.length; j++) {
+            var ingredient = element.ingredients[j];
+            ingredients += "Ingredient Name: " + ingredient.name;
+            ingredients += "Quantity: " + ingredient.quantity;
+            ingredients += ", "
+        }
+
+        ingredients += "}";
+
+        output += ingredients;
+
+    }
+
+    document.getElementById("recipes").innerHTML = output;
+
+}
+
+window.onload = listRecipes;
---
+++ APC_P3_AnaClaraRibas/script/function.js
@@ -0,0 +1,79 @@
+﻿function navbar() {
+    document.getElementById("navbar-content").innerHTML = 
+        `<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="masthead mb-auto">
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                <a class="navbar-brand" href="index.html" target="_self"><img src="../content/platejoy-logo2.png" alt="" height="36" width="78" /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto" id="buttons-navbar-left">
+                        <li class="nav-item">
+                            <a class="nav-link" href="form-recipes.html">Recipes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.mealime.com/">Meal Planner</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.mealime.com/kitchen-essentials-list">Cookware</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.platejoy.com/blog">Blog</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="form-login.html">Sign In<span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2" id="searchbar-textbox" type="text" placeholder="Search and Filter" aria-label="Search and Filter">
+                    <button class="btn btn-outline-info my-2 my-sm-0" id="button-search-navbar" type="submit">Search</button>
+                </form>
+            </nav>
+        </header>`;
+}
+
+function footer() {
+    document.getElementById("footer-content").innerHTML = 
+        `<footer class="my-5 pt-5 text-muted text-center text-small">
+            <p>Bootstrap by <a href="https://www.instagram.com/aclararibas/">aclararibas</a>.</p>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Privacy</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Support</a></li>
+            </ul>
+        </footer>`;
+}
+
+window.onload = init;
+
+function init() {
+    navbar();
+    footer();
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+    }, false)
+}())
+
+
